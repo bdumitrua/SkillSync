e2eb3caf5921d0a2a736bdbb63a05e41ede3cc0e
bdumitrua <bdumitrua@gmail.com>
2024-03-24 18:36:30 +0300

    sm fix

--- services/teams-service/Dtos/Team/CreateTeamRequestDto.cs
+++ services/teams-service/Dtos/Team/CreateTeamRequestDto.cs
@@ -2,7 +2,6 @@ namespace TeamsService.Dtos.Team
 {
     public class CreateTeamRequestDto
     {
-        public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
     }
 }
