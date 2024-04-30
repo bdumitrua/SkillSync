3c6560a388f88e370313284de65246fbb0c908ac
bdumitrua <bdumitrua@gmail.com>
2024-04-30 13:22:54 +0300

    added docker and requirements

--- services/posts_service/posts_service/settings.py
+++ services/posts_service/posts_service/settings.py
@@ -75,9 +75,17 @@ WSGI_APPLICATION = 'posts_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'posts_db',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': 'posts-db',
+        'PORT': '3306',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
