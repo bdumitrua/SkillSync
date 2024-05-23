bf1481430dd4eac4608cf82a4f5cf09f83809415
bdumitrua <bdumitrua@gmail.com>
2024-05-24 00:28:37 +0300

    rm tr

--- update-node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Получаем ID контейнера
-CONTAINER_ID=$(docker-compose ps -q frontend)
-
-# Копируем папку vendor на локальную машину
-docker cp $CONTAINER_ID:/app/frontend/node_modules ./frontend/
