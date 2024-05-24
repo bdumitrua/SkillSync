f9634523282dac3a855473e70f6fb0a2d85815dd
bdumitrua <bdumitrua@gmail.com>
2024-05-24 16:25:53 +0300

    moved firebase logic and connection to laravel; created routes

---
+++ services/users-service/routes/messages.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\ChatController;
+use App\Http\Controllers\MessageController;
+use Illuminate\Support\Facades\Route;
+
+// /messages
+Route::prefix('messages')->middleware(['auth:api'])->group(function () {
+    Route::controller(MessageController::class)->group(function () {
+        Route::post('send/{chatId}', 'send')->name('messages.send');
+        Route::post('read/{chatId}/{messageUuid}', 'read')->name('messages.read');
+        Route::delete('delete/{chatId}/{messageUuid}', 'delete')->name('messages.delete');
+    });
+
+    // /messages/chats
+    Route::prefix('chats')->controller(ChatController::class)->group(function () {
+        Route::get('index', 'index')->name('messages.chats.index');
+        Route::get('show/{chatId}', 'show')->name('messages.chats.show');
+        Route::post('/', 'create')->name('messages.chats.create');
+
+        // /messages/chats/members
+        Route::prefix('members')->controller(ChatController::class)->group(function () {
+            Route::get('{chatId}', 'show')->name('messages.chats.members.show');
+            Route::post('{chatId}/{userId}', 'add')->name('messages.chats.members.add');
+            Route::delete('{chatId}/{userId}', 'remove')->name('messages.chats.members.remove');
+        });
+    });
+});
